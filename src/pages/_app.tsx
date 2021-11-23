7df962feca90a1fd832e649313f7151253679e9f
osakana0630 <osakana0630@gmail.com>
2021-11-23 15:17:50 +0900

    不要ファイル削除/antd使用できるように

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../components/Layout/Layout";
+import 'antd/dist/antd.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import {
+  Button
+} from "antd";
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello Next.js 👋</h1>
+    <Button type="primary">antdのボタン</Button>
+  </div>
+
+)
+
+export default IndexPage
